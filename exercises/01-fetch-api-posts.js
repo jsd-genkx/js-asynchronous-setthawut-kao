812cb534d794faf3f836629c20a2882968c04fee
setthawut-kao <setthawut.kao@gmail.com>
2025-07-24 15:35:06 +0700

    exercise01(js dummyApiPost

--- exercises/01-fetch-api-posts.js
+++ exercises/01-fetch-api-posts.js
@@ -5,5 +5,25 @@ TODO-01: Create a function named fetchPostsThen
          Use another .then() to log the posts data
          Use .catch() to handle errors
 
-TODO-02: Export fetchPostsThen and test it in main.js 
+TODO-02: Export fetchPostsThen and test it in main.js
 */
+
+export
+const fetchPostsThen = () => {
+	const endpoint = "https://dummyjson.com/posts";
+	fetch(endpoint)
+		.then((res) => {
+			if (!res.ok) {
+				throw new Error("Failed to fetch posts");
+			}
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data.posts[0]);
+		})
+		.catch((error) => {
+			console.log("Error occurred: ", error);
+		});
+};
+
+// fetchPostsThen();
--- main.js
+++ main.js
@@ -14,11 +14,13 @@ import { asyncAwaitBasic } from "./13-async-await-basic.js";
 import { fetchDataAsync } from "./14-fetch-data-async.js";
 import { fetchDataAsyncSingle } from "./15-try-catch-finally.js";
 
+import { fetchPostsThen } from "./exercises/01-fetch-api-posts.js";
+
 const runButton = document.getElementById("runCodeBtn");
 
 runButton.addEventListener("click", () => {
 	/* 01-sync-basic.js */
-	syncBasic();
+	// syncBasic();
 	/* 02-sync-blocking.js */
 	// syncBlocking();
 	/* 03-sync-optimize.js */
@@ -47,4 +49,9 @@ runButton.addEventListener("click", () => {
 	// fetchDataAsync();
 	/* 15-try-catch-finally.js */
 	// fetchDataAsyncSingle(10);
+
+	// exercises01
+	fetchPostsThen();
+
+
 });
